a1a8363d67d3f815e2d84a2f1f4b55f85bf6a4e9
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-09 13:20:50 +0500

    Add data types example

---
+++ 01-Basics/09-data-types.js
@@ -0,0 +1,11 @@
+let fullName = "Abdullah";
+let age = 23;
+let isStudent = true;
+let city = null;
+let country;
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof country);
