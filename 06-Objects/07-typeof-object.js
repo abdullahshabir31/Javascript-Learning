269229c7e54fc4f01121cc70004a5f480a62d73f
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-10 15:12:16 +0500

    Add typeof operator with objects example

---
+++ 06-Objects/07-typeof-object.js
@@ -0,0 +1,10 @@
+const profile = {
+  username: "abdullahshabir31",
+  followers: 256,
+  following: 67,
+  isFollow: true,
+};
+
+console.log(typeof profile);
+console.log(typeof profile.username);
+console.log(typeof profile.followers);
