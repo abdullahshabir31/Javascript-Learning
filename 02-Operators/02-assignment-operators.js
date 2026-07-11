bfe42fccb5231a16ff246b2f17afc74afc71d98b
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-11 12:39:02 +0500

    Add assignment operators example

---
+++ 02-Operators/02-assignment-operators.js
@@ -0,0 +1,16 @@
+let number = 10;
+
+number += 5;
+console.log(number);
+
+number -= 2;
+console.log(number);
+
+number *= 3;
+console.log(number);
+
+number /= 2;
+console.log(number);
+
+number %= 4;
+console.log(number);
