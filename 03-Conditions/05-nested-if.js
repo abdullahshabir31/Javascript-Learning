e0b2ea743b2686249347ffdef9957db924c53a1c
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-12 19:26:07 +0500

    Add nested if statement example

---
+++ 03-Conditions/05-nested-if.js
@@ -0,0 +1,12 @@
+let age = 20;
+let hasLicense = true;
+
+if (age >= 18) {
+  if (hasLicense) {
+    console.log("You can drive.");
+  } else {
+    console.log("Get a driving license first.");
+  }
+} else {
+  console.log("You are underage.");
+}
