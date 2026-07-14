101492c4b1b495be97bef67d4029fbf87b8a9c15
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-14 12:31:46 +0500

    Add prime number example

---
+++ 04-Loops/14-prime-number.js
@@ -0,0 +1,17 @@
+// Example: Prime Number Check
+
+let number = 17;
+let isPrime = true;
+
+for (let i = 2; i < number; i++) {
+  if (number % i === 0) {
+    isPrime = false;
+    break;
+  }
+}
+
+if (isPrime) {
+  console.log(number + " is a prime number.");
+} else {
+  console.log(number + " is not a prime number.");
+}
