b88cdbba9ba13eedc377f0a5a8ff7e8d9897084b
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-16 23:54:10 +0500

    Add recursive factorial example

---
+++ 05-Functions/09-recursive-factorial.js
@@ -0,0 +1,11 @@
+// Example: Recursive Factorial
+
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(5));
