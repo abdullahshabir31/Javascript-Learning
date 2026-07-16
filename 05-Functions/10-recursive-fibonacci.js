6056861b7b702264606ddcd4124dc1d5713754ad
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-16 23:54:30 +0500

    Add recursive Fibonacci example

---
+++ 05-Functions/10-recursive-fibonacci.js
@@ -0,0 +1,13 @@
+// Example: Recursive Fibonacci
+
+function fibonacci(number) {
+  if (number <= 1) {
+    return number;
+  }
+
+  return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(fibonacci(i));
+}
