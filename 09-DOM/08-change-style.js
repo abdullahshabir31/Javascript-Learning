ce6dfda54e4d144a4e7496156a9245c71c207fbb
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-21 13:34:26 +0500

    Add change style example

---
+++ 09-DOM/08-change-style.js
@@ -0,0 +1,7 @@
+// Example: Change CSS Style
+
+const heading = document.getElementById("heading");
+
+heading.style.color = "blue";
+heading.style.backgroundColor = "yellow";
+heading.style.fontSize = "40px";
