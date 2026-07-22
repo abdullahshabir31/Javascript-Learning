a0bb0e75765d88a24f1a682f5872ccab06a59d78
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-22 12:53:45 +0500

    Add appendChild example

---
+++ 09-DOM/12-append-element.js
@@ -0,0 +1,9 @@
+// Example: appendChild()
+
+const container = document.getElementById("container");
+
+const paragraph = document.createElement("p");
+
+paragraph.textContent = "This paragraph was added using JavaScript.";
+
+container.appendChild(paragraph);
