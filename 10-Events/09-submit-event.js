663056b7f43da272396c2945cb5f8cf28ab81894
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-25 12:51:28 +0500

    Add submit event example

---
+++ 10-Events/09-submit-event.js
@@ -0,0 +1,8 @@
+// Example: Submit Event
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert("Form Submitted!");
+});
