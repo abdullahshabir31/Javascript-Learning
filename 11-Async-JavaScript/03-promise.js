5cefe4028f9cbc6d4865a3c155867294e2624afb
Abdullah Shabir <abdullahshabir31@gmail.com>
2026-07-26 13:18:17 +0500

    Add promise example

---
+++ 11-Async-JavaScript/03-promise.js
@@ -0,0 +1,15 @@
+// Example: Promise
+
+const promise = new Promise((resolve, reject) => {
+  const success = true;
+
+  if (success) {
+    resolve("Promise Resolved");
+  } else {
+    reject("Promise Rejected");
+  }
+});
+
+promise
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
